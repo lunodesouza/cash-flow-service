b6056d35b97af6b145f6c0b2fa6a5ca2f6656eb9
Luno <lunodesouza@gmail.com>
2023-05-18 11:40:44 -0300

    Initial project setup

---
+++ src/test/java/com/github/lunodesouza/CashFlowServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.lunodesouza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashFlowServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
